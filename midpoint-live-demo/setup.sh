2a3b35b10a635287859e5dec2219b9a53db121bf
jmederly <jan.mederly@evolveum.com>
2022-02-28 21:37:42 +0100

    Changed structure and added documentation.

--- midpoint-live-demo/setup.sh
+++ midpoint-live-demo/setup.sh
@@ -1,18 +1,46 @@
 #!/bin/bash
 
+# Import of namespaces and nfs objects
 kubectl apply -f $PWD/namespaces
 kubectl apply -f $PWD/objects/nfs
 
-kubectl create configmap -n mp-demo pio-map --from-file=$PWD/config-maps/mp-pio
-kubectl create configmap -n mp-demo extension-map --from-file=$PWD/config-maps/mp-extensions
-kubectl create configmap -n mp-demo hr-map --from-file=$PWD/config-maps/mp-hr
-kubectl create configmap -n mp-demo addressbook-script-map --from-file=$PWD/config-maps/addressbook
-kubectl create configmap -n mp-demo hr-script-map --from-file=$PWD/config-maps/hr
-kubectl create configmap -n mp-demo apache-map --from-file=$PWD/config-maps/apache
-kubectl create configmap -n mp-demo ldap-init-map --from-file=$PWD/config-maps/ldap-init
-kubectl create configmap -n mp-demo ldap-dynschema-map --from-file=$PWD/config-maps/ldap-dynschema
+# Import of midpoint internal objects in config maps. These are extensions, post initial objects and initial hr resources.
+kubectl create configmap -n mp-demo pio-map --from-file=$PWD/objects/mp/config-map/mp-post-initial-objects
+kubectl create configmap -n mp-demo extension-map --from-file=$PWD/objects/mp/config-map/mp-extensions
+kubectl create configmap -n mp-demo hr-map --from-file=$PWD/objects/mp/config-map/mp-hr
 
-kubectl apply -Rf $PWD/objects
+# Import of midpoint kubernetes objects
+kubectl apply -f $PWD/objects/mp/yamls
+
+# Import adressbook database initial script as config map 
+kubectl create configmap -n mp-demo addressbook-script-map --from-file=$PWD/objects/addressbook/config-map
+
+# Import of adressbook kubernetes objects
+kubectl apply -f $PWD/objects/addressbook/yamls
+
+# Import of hr database initial script as config map
+kubectl create configmap -n mp-demo hr-script-map --from-file=$PWD/objects/hr/config-map
+
+# Import of hr kubernetes objects
+kubectl apply -f $PWD/objects/hr/yamls
+
+# Import of httpd.conf file for apache server as config map
+kubectl create configmap -n mp-demo apache-map --from-file=$PWD/objects/apache/config-map
+
+# Import of apache server kubernetes objects
+kubectl apply -f $PWD/objects/apache/yamls
+
+# Import of ldap initial objects as config map
+kubectl create configmap -n mp-demo ldap-init-map --from-file=$PWD/objects/ldap-init/config-map
+
+# Import of dynschema for ldap as config map
+kubectl create configmap -n mp-demo ldap-dynschema-map --from-file=$PWD/objects/ldap-dynschema/config-map
+
+# Import of ldap kuberntes objects
+kubectl apply -f $PWD/objects/ldap/yamls
+
+# Import od tomcat kubernetes objects
+kubectl apply -f $PWD/objects/tomcat
 
 
 
