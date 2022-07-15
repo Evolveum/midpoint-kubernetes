c7fc2047baa41c0dda284c1d9f3ca6afbba0253c
jmederly <jan.mederly@evolveum.com>
2022-07-15 11:28:07 +0200

    Add an option for a custom namespace.

--- midpoint-live-demo/midpoint-live-demo-setup.sh
+++ midpoint-live-demo/midpoint-live-demo-setup.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
-kubectl apply -k ./base/ -n mp-demo
+while getopts n:s:h flag
+do
+    case "${flag}" in
+        n) NAMESPACE=${OPTARG};;
+        s) SERVER=${OPTARG};;
+        h | *)
+          echo "script usage:"
+          echo "$0 [-s value] [-n value]"
+          echo "-n 	 custom namespace in which demo would be deployed. Default namespace is mp-demo"
+          echo "-s 	 custom nfs server address. Default is nfs-service.<YOUR NAMESPACE>.svc.cluster.local"
+          ;; 
+    esac
+done
+
+if [ -n $SERVER ]
+then
+   sed -i "s/nfs_server: .*/nfs_server: $SERVER/g" base/nfs-server-map.yaml
+elif [ -n $NAMESPACE ]
+then
+   sed -i "s/nfs_server: .*/nfs_server: nfs-service.$NAMESPACE.svc.cluster.local/g" base/nfs-server-map.yaml
+fi
+
+if [ -n $NAMESPACE ]
+then
+   sed -i "s/name: .*/name: $NAMESPACE/g" base/namespaces/namespace.yml
+   kubectl apply -k ./base/ -n $NAMESPACE
+else
+   kubectl apply -k ./base/ -n mp-demo
+fi
