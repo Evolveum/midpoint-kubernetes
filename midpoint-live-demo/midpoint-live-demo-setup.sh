fc79155e64903a99a6e53895ab97fa8b5bdd3fce
jan <jan.mederly@gmail.com>
2022-08-08 23:43:46 +0200

    Update link on mp login page, update startup script and readme

--- midpoint-live-demo/midpoint-live-demo-setup.sh
+++ midpoint-live-demo/midpoint-live-demo-setup.sh
@@ -3,11 +3,10 @@
 NAMESPACE=mp-demo
 HOST=demo.example.com
 
-while getopts n:s:c:a:i:h flag
+while getopts n:c:a:i:h flag
 do
     case "${flag}" in
         n) NAMESPACE=${OPTARG};;
-        s) SERVER=${OPTARG};;
         c) CERTADDRESS=${OPTARG};;
         a) HOST=${OPTARG};;
         i) INGRESSCLASS=${OPTARG};;
@@ -15,7 +14,6 @@ do
           echo "script usage:"
           echo "$0 [-s value] [-n value]"
           echo "-n 	 custom namespace in which demo would be deployed. Default namespace is mp-demo"
-          echo "-s 	 custom nfs server address. Default is nfs-service.<YOUR NAMESPACE>.svc.cluster.local"
           echo "-c 	 custom yaml certificate for ingresses. Default certificate is packed with demo. PLEASE USE THE SAME NAME FOR THE FILE AND THE KUBERNETES OBJECT"
           echo "-a 	 custom host for ingresses. Default host is demo.example.com. Please be aware that default certificate does work only with default host"
           echo "-i    custom ingressClass name. Default is default on your machine (one with annotation ingressclass.kubernetes.io/is-default-class: true). Make sure that you do have ONE default ingressClass or use this option to set a custom one."
@@ -55,13 +53,6 @@ EOF
 fi
 
 sed -i "s/ingress_cert: .*/ingress_cert: $CERT/g" kustomize-env-config/options-map.yaml
-
-if [ $SERVER ]
-then
-   sed -i "s/nfs_server: .*/nfs_server: $SERVER/g" kustomize-env-config/options-map.yaml
-else
-   sed -i "s/nfs_server: .*/nfs_server: nfs-service.$NAMESPACE.svc.cluster.local/g" kustomize-env-config/options-map.yaml
-fi
 
 kubectl create namespace $NAMESPACE 2> /dev/null || true
 kubectl apply -k . -n $NAMESPACE
