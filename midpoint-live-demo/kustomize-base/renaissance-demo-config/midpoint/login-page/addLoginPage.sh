afd9e2ff9b161561da361799b98a6a943cbc6afc
Slavek Licehammer <slavek@evolveum.com>
2024-08-21 13:49:07 +0200

    Upgrade demo to 4.8.4

--- midpoint-live-demo/kustomize-base/renaissance-demo-config/midpoint/login-page/addLoginPage.sh
+++ midpoint-live-demo/kustomize-base/renaissance-demo-config/midpoint/login-page/addLoginPage.sh
@@ -3,15 +3,15 @@ cp /opt/midpoint/lib/* /mnt/lib/
 cd /mnt/lib
 unzip -q midpoint.jar -d midpoint2
 mkdir /mnt/gui/
-cp midpoint2/BOOT-INF/lib/admin-gui-4.8.3.jar /mnt/gui/
+cp midpoint2/BOOT-INF/lib/admin-gui-4.8.4.jar /mnt/gui/
 cd /mnt/gui/
-unzip -q admin-gui-4.8.3.jar -d gui2
+unzip -q admin-gui-4.8.4.jar -d gui2
 cp -f /mnt/page/AbstractPageLogin.html gui2/com/evolveum/midpoint/gui/impl/page/login/AbstractPageLogin.html
-rm -f admin-gui-4.8.3.jar
+rm -f admin-gui-4.8.4.jar
 apk add zip
 cd gui2
-zip -0 -r admin-gui-4.8.3.jar .
-cp -f admin-gui-4.8.3.jar /mnt/lib/midpoint2/BOOT-INF/lib/
+zip -0 -r admin-gui-4.8.4.jar .
+cp -f admin-gui-4.8.4.jar /mnt/lib/midpoint2/BOOT-INF/lib/
 echo "Login Page update - default demo login information" >/mnt/lib/midpoint2/overlay-info.txt
 cd /mnt/lib
 rm midpoint.jar
